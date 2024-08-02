0917a367a102ef1013e5cf676ecf62eadc03e645
nsandoya <natsandoya@gmail.com>
2024-08-02 10:12:56 -0500

    Fetch and json changes (post-challenge)

--- src/services/getHotels.js
+++ src/services/getHotels.js
@@ -1,5 +1,5 @@
-export const getHotels = async () => {
-    const url = '../database/hotels.json';
+const getHotels = async () => {
+    const url = 'https://github.com/nsandoya/blr_hotels_tipti_challenge/blob/main/src/database/hotels.json';
     const options = {
         method: 'GET',
         headers: {
@@ -9,10 +9,13 @@ export const getHotels = async () => {
     try {
         const response = await fetch(url);
         const result = await response.json();
-        console.log(result);
+        console.log("Datos obtenidos", result);
         return result
     } catch (error) {
         console.error(error);
     }
 
 } 
+
+//export default getHotels
+getHotels()
